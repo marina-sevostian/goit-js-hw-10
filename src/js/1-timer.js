7b380689426ecaa72ec7a8ea1f10206d1125684f
Marina.S. <marina.sevostian@gmail.com>
2024-10-19 05:42:43 +0300

    add timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,54 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const elements = {
+  startBtn: document.querySelector('[data-start]'),
+  dateInput: document.querySelector('input#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+elements.startBtn.addEventListener('clik', onStartBtnCountdown);
+
+elements.startBtn.disabled = true;
+
+let userSelectedDate = '';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      iziToast.show({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        color: '#ef4040',
+      });
+      // return;
+    } else {
+      elements.startBtn.disabled = false;
+      // console.log(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(elements.dateInput, options);
+
+let intervalId = 0;
+
+function onStartBtnCountdown() {
+  intervalId = setInterval(createTimer, 1000);
+  elements.startBtn.disabled = true;
+  elements.dateInput.disabled = true;
+}
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -21,17 +72,33 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+function createTimer() {
+  const totalMs = userSelectedDate - new Date();
+  let timeObject = convertMs(totalMs);
+  elements.days.textContent = addLeadingZero(timeObject.days);
+  elements.hours.textContent = addLeadingZero(timeObject.hours);
+  elements.minutes.textContent = addLeadingZero(timeObject.minutes);
+  elements.seconds.textContent = addLeadingZero(timeObject.seconds);
+  // const dayPicker = addLeadingZero(timeObject.days);
+  // const hoursPicker = addLeadingZero(timeObject.hours);
+  // const minutesPicker = addLeadingZero(timeObject.minutes);
+  // const secondsPicker = addLeadingZero(timeObject.seconds);
+  // elements.days.textContent = addLeadingZero(dayPicker);
+  // elements.days.textContent = dayPicker;
+  // elements.hours.textContent = hoursPicker;
+  // elements.minutes.textContent = minutesPicker;
+  // elements.seconds.textContent = secondsPicker;
+
+  if (minutesPicker === 0 && secondsPicker === 0) {
+    clearTimer();
+  }
+}
+
+function clearTimer() {
+  clearInterval(intervalId);
+  elements.startBtn.disabled = true;
+}
