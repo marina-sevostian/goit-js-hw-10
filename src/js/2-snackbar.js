1a60290eba5c1587e3073ca339c5cfcea92ab660
Marina.S. <marina.sevostian@gmail.com>
2024-10-19 11:18:22 +0300

    add message snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,55 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+iziToast.success({
+  title: 'OK',
+  message: '✅ Fulfilled promise in ${delay}ms',
+  color: '#59a10d',
+  timeout: '${delay}',
+  //   close: true,
+});
+iziToast.error({
+  title: 'Error',
+  message: '❌ Rejected promise in ${delay}ms',
+  color: '#ef4040',
+  timeout: '${delay}',
+  //   close: true,
+});
+iziToast.warning({
+  title: 'Caution',
+  message: 'You forgot important data',
+  color: '#ffa000',
+  //   need position left for close
+  close: true,
+});
+iziToast.info({
+  // need add link icon
+  icon: './img/informing-icon.svg',
+  iconColor: 'none',
+  title: 'Hello',
+  message: 'Welcome!',
+  color: '#09f',
+  //   close: true,
+});
+iziToast.success({
+  image: './img/mes-foto.jpg',
+  imageWidth: 88,
+  title: 'Hello',
+  message: 'Do you like it?',
+  color: '#565c72',
+  //   close: true,
+});
+iziToast.success({
+  image: './img/mes-foto.jpg',
+  imageWidth: 56,
+  title: 'Hey',
+  message: 'What would you like to add?',
+  color: '#f0f0f0',
+  balloon: true,
+  buttons: [
+    ['<button>Photo</button>', 68],
+    ['<button>Video</button>'],
+    ['<button>Text</button>'],
+  ],
+  //   close: true,
+});
